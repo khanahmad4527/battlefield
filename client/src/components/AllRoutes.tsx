1ba27600ab5e71dcc959b15712905f3c473c9a89
akash-singha-3558 <akashsingha@gmail.com>
2023-04-08 23:29:49 +0530

    hoemapge added with logo

--- client/src/components/AllRoutes.tsx
+++ client/src/components/AllRoutes.tsx
@@ -3,12 +3,14 @@ import { Route, Routes } from "react-router-dom";
 import Game from "./Game";
 import PageNotFound from "./PageNotFound";
 import Home from "./HomePage";
+import Help from "./Help";
 
 const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/game" element={<Game />} />
+      <Route path="/help" element={<Help/>} />
       <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
--- client/src/components/HomePage.tsx
+++ client/src/components/HomePage.tsx
@@ -4,13 +4,19 @@ import HomeCard from './Home.Card'
 import { AiOutlineRight } from "react-icons/ai";
 import "./text.css"
 import { useNavigate } from "react-router-dom";
+import AudioPlayer from './AudioPlayer';
 const Home = () => {
   const navigate = useNavigate();
   return (
     <div  style={{width:"100%",height:"100vh",
     background: "linear-gradient(90deg, rgba(13,11,41,1) 32%, rgba(105,17,17,1) 77%, rgba(38,69,75,1) 100%)",display:"flex",justifyContent:"center",alignItems:"center",flexDirection:"column",gap:"10px"}} >
    <HomeCard/>
-   <p className='text'  onClick={() => navigate("/game")} >Start <AiOutlineRight/></p>
+   <p className='text'  onClick={() => navigate("/game")} >Start <AiOutlineRight /></p>
+   <p style={{cursor:"pointer",textDecoration:"underline"}} onClick={() => navigate("/help")}>Need Help ?</p>
+   {/* <audio id="musicplayer" autoPlay>
+  <source src="https://www.bensound.com/royalty-free-music/track/lofi-nerd-bass-buzzer-cool-fuzz-bass" />
+</audio> */}
+<AudioPlayer/>
     </div>
   )
 }
