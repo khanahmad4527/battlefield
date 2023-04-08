0bea39edcc3c4fb3051026b4e9402b0e4ea5a0a3
akash-singha-3558 <akashsingha@gmail.com>
2023-04-08 20:12:08 +0530

    hoemapge added with logo

--- client/src/components/AllRoutes.tsx
+++ client/src/components/AllRoutes.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Game from "./Game";
 import PageNotFound from "./PageNotFound";
-import HomePage from "./HomePage";
+import Home from "./HomePage";
 
 const AllRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<Home />} />
       <Route path="/game" element={<Game />} />
       <Route path="*" element={<PageNotFound />} />
     </Routes>
--- client/src/components/HomePage.tsx
+++ client/src/components/HomePage.tsx
@@ -1,14 +1,18 @@
-import React from "react";
-import { Button } from "@chakra-ui/react";
+import React from 'react'
+import HomeCard from './Home.Card'
+// AiOutlineRight
+import { AiOutlineRight } from "react-icons/ai";
+import "./text.css"
 import { useNavigate } from "react-router-dom";
-const HomePage = () => {
+const Home = () => {
   const navigate = useNavigate();
   return (
-    <div>
-      HomePage
-      <Button onClick={() => navigate("/game")}>Game Page</Button>
+    <div  style={{width:"100%",height:"100vh",
+    background: "linear-gradient(90deg, rgba(13,11,41,1) 32%, rgba(105,17,17,1) 77%, rgba(38,69,75,1) 100%)",display:"flex",justifyContent:"center",alignItems:"center",flexDirection:"column",gap:"10px"}} >
+   <HomeCard/>
+   <p className='text'  onClick={() => navigate("/game")} >Start <AiOutlineRight/></p>
     </div>
-  );
-};
+  )
+}
 
-export default HomePage;
+export default Home
