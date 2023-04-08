63d0ea99f3d4c3986d8a02acb3f35f33782d0d83
akash-singha-3558 <akashsingha@gmail.com>
2023-04-09 00:49:41 +0530

    audio added

--- client/src/components/AudioPlayer.tsx
+++ client/src/components/AudioPlayer.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
-// import audio from "./audio.mp3"
-const AudioPlayer = () => {
-  let srcid="https://drive.google.com/uc?id=1Dis7qHbE2-xwAO5Z2vNFsD2XUh4vNYwr"
+// 
+import { MdMusicNote,MdMusicOff } from "react-icons/md";
+import React, { useState, useEffect } from 'react';
+function AudioPlayer() {
+  const [audio] = useState(new Audio('https://drive.google.com/uc?id=1Dis7qHbE2-xwAO5Z2vNFsD2XUh4vNYwr'));
+  const [audioPlaying, setAudioPlaying] = useState(false);
+
+  const handlePlay = () => {
+    audio.play();
+    setAudioPlaying(true);
+  };
+
+  const handlePause = () => {
+    audio.pause();
+    setAudioPlaying(false);
+  };
+
   return (
     <div>
-<audio src={"https://drive.google.com/uc?id=1Dis7qHbE2-xwAO5Z2vNFsD2XUh4vNYwr"} autoPlay loop />
+      <button onClick={audioPlaying ? handlePause : handlePlay} style={{border:"1px solid white",padding:"5px",borderRadius:"15PX"}}>
+        {audioPlaying ? <MdMusicOff/> : <MdMusicNote/>}
+      </button>
     </div>
-    
   );
-};
+}
+
+
+
+
 
 export default AudioPlayer;
